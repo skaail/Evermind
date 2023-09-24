fa8de9c650d1fc06c107d1f7746f12a5158a569e
Bruno PC <blgianini@gmail.com>
2023-09-24 16:53:52 -0300

    primeiro commit

---
+++ src/test/java/br/com/evermind/Evermind/EvermindApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evermind.Evermind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvermindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
