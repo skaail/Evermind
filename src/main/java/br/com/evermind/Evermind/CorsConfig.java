4dc59aa6cf36e7347147430f3e0d6a66fc987757
Bruno PC <blgianini@gmail.com>
2023-09-24 21:49:02 -0300

    Cors new

---
+++ src/main/java/br/com/evermind/Evermind/CorsConfig.java
@@ -0,0 +1,19 @@
+package br.com.evermind.Evermind;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Requestor-Type")
+                .exposedHeaders("X-Get-Header")
+                .maxAge(3600);
+    }
+}
